3abf0e9a5bf5983563c9847e69b4a72870e7d7f1
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-22 17:06:02 +0100

    Update zoo.js
    pdate to output  wip

--- zoo.js
+++ zoo.js
@@ -3,8 +3,11 @@ var suits = ["spades", "hearts", "clubs", "diams"];
 var numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var output = document.getElementById("output");
 for (let s in suits) {
+    let suit = suits[s][0].toUpperCase;
+    let bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
     for (let n in numb) {
-        output.innerHTML += suits[s] + numb[n];
+        output.innerHTML += "<span style= 'color: " + bgcolor + " ' > ";
+        output.innerHTML += " & " + suits[s] + " , " + numb[n] + " </span> ";
     }
 }
 
