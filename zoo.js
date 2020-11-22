7bd2de80536a9a0bb4ff5381b387e6d5d8d60ba2
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-22 17:53:28 +0100

    Update zoo.js
    made card arrays print out with the sign from w3 shools

--- zoo.js
+++ zoo.js
@@ -3,12 +3,21 @@ var suits = ["spades", "hearts", "clubs", "diams"];
 var numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var output = document.getElementById("output");
 for (let s in suits) {
-    let suit = suits[s][0].toUpperCase;
-    let bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
+    var suit = suits[s][0].toUpperCase();
+    var bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
     for (let n in numb) {
-        output.innerHTML += "<span style= 'color: " + bgcolor + " ' > ";
-        output.innerHTML += " & " + suits[s] + " , " + numb[n] + " </span> ";
+        //    output.innerHTML += "<span style='color:" + bgcolor + "'>&" + suits[s] + ";" + numb[n] + "</span> ";
+        let cardValue = (n > 9) ? 10 : Number(n) + 1;
+        let card = {
+            suit: suit,
+            icon: suits[s],
+            bgcolor: bgcolor,
+            cardnum: numb[n],
+            cardalue: cardValue
+        };
+        cards.push(card);
     }
 }
-
-var randomNum = Math.floor((Math.random() * 52) + 1);
+console.log(cards);
+let randomNum = Math.floor((Math.random() * 52));
+output.innerHTML += "<span style='color:" + cards[randomNum].bgcolor + " '> &" + cards[randomNum].icon + ";" + cards[randomNum].cardnum + "</span> ";
