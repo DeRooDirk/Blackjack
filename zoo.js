9103e0887221d4c3faa99edd7655c4ac7b4296e1
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-22 16:03:23 +0100

    update js and html
    reworked output to html

--- zoo.js
+++ zoo.js
@@ -1,10 +1,10 @@
 var cards = [];
 var suits = ["spades", "hearts", "clubs", "diams"];
 var numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+var output = document.getElementById("output");
 for (let s in suits) {
     for (let n in numb) {
-        console.log(suits[s] + numb[n]);
+        output.innerHTML += suits[s] + numb[n];
     }
 }
 
