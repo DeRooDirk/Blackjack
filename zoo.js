cb35bb82e86e2ae3ef047174bf062a962e9d67ba
Zoo Wie <de-roo.dirk@hotmail.com>
2020-11-22 19:18:17 +0100

    Update zoo.js
    the Cards are now beeing printed to the html ,

--- zoo.js
+++ zoo.js
@@ -1,3 +1,4 @@
+// Creating Variables and arrays to holdstuff in 
 var cards = [];
 var playerCard = [];
 var dealerCard = [];
@@ -7,13 +8,14 @@ var numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var output = document.getElementById("output");
 var dealerHolder = document.getElementById("dealerHolder");
 var playerHolder = document.getElementById("playerHolder");
+
 for (let s in suits) {
     var suit = suits[s][0].toUpperCase();
     var bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
     for (let n in numb) {
-        //    output.innerHTML += "<span style='color:" + bgcolor + "'>&" + suits[s] + ";" + numb[n] + "</span> ";
-        let cardValue = (n > 9) ? 10 : Number(n) + 1;
-        let card = {
+        //output.innerHTML += "<span style='color:" + bgcolor + "'>&" + suits[s] + ";" + numb[n] + "</span> ";
+        var cardValue = (n > 9) ? 10 : Number(n) + 1;
+        var card = {
             suit: suit,
             icon: suits[s],
             bgcolor: bgcolor,
@@ -23,17 +25,13 @@ for (let s in suits) {
         cards.push(card);
     }
 }
-
-
+// start function
 function start() {
-
     shuffleDeck(cards);
-
     dealNew();
-    outputCard();
 
 }
-
+// deal a new deck of cards 
 function dealNew() {
     playerCard = [];
     dealerCard = [];
@@ -41,16 +39,23 @@ function dealNew() {
     playerHolder.innerHTML = "";
     for (x = 0; x < 2; x++) {
         dealerCard.push(cards[cardCount]);
+        dealerHolder.innerHTML += cardOutput(cardCount);
         cardCount++;
         playerCard.push(cards[cardCount]);
+        playerHolder.innerHTML += cardOutput(cardCount);
         cardCount++;
     }
     console.log(dealerCard);
     console.log(playerCard);
-}
 
+}
+//printing to html
+function cardOutput(n) {
+    return "<span style='color:" + cards[cardCount].bgcolor + "'>" + cards[cardCount].cardnum + "&" + cards[cardCount].icon + ";</span>  ";
+}
+// to go tru the array backwards 
 function shuffleDeck(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
         array[i] = array[j];
@@ -58,9 +63,9 @@ function shuffleDeck(array) {
     }
     return array;
 }
-
+//printing to html 
 function outputCard() {
-    output.innerHTML += "<span style='color:" + cards[cardCount].bgcolor + "'> &" + cards[cardCount].icon + ";" + cards[cardCount].cardnum + "</span>     ";
+    output.innerHTML += "<span style='color:" + cards[cardCount].bgcolor + "'>" + cards[cardCount].cardnum + "&" + cards[cardCount].icon + ";</span>  ";
 }
 
 
@@ -72,6 +77,11 @@ function outputCard() {
 /*
 
  let randomNum = Math.floor((Math.random() * 52));
+
 	output.innerHTML += "<span style='color:" + cards[randomNum].bgcolor + "'> &" + cards[randomNum].icon + ";" + cards[randomNum].cardnum + "</span>     ";
+
+Math.floor(Math.random() * (i + 1));
+
+	 output.innerHTML += "<span style='color:" + cards[cardCount].bgcolor + "'>" + cards[cardCount].cardnum + "&" + cards[cardCount].icon + ";</span>  ";
 	
 	*/
